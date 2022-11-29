c396cc1ccc64328a8cfeb29bf980cf378c857609
Pessi Vilppolahti <pessiv@outlook.com>
2022-11-29 00:27:36 +0000

    added symbolic links for ease of consumption

--- cookinit.sh
+++ cookinit.sh
@@ -4,3 +4,5 @@ git clone https://github.com/Donearm/Cooking-Recipes.git
 git clone https://github.com/LukeSmithxyz/based.cooking.git
 cd ..
 find . | grep ".md" > recipelist.txt
+ln -s ./dependencies/based.cooking/content .
+ln -s ./dependencies/Cooking-Recipes/recipes .
